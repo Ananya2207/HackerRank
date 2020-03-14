f2897059e27ee331423cd717f8667cf61520177a
Ananya <ananya@rknec.edu>
2020-03-14 19:24:45 +0530

    Create Day16Exceptions.java

---
+++ Day16Exceptions/Day16Exceptions.java
@@ -0,0 +1,24 @@
+//package hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day16Exceptions {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        
+        try{
+        int num=Integer.parseInt(S);
+            System.out.println(num);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Bad String");
+        }
+    }
+}
